9165631095d1f56a379c7367a86e2b64fe6b9ed1
chef-pepe <ap334@disroot.org>
2021-02-09 21:50:44 -0800

    Building out the bot service itself

--- shillbot.py
+++ shillbot.py
@@ -2,6 +2,57 @@ import os
 
 import tweepy
 
+FIRM_TO_BAGS = {
+    '@paradigm': {
+        'uniswap',
+        'optimism',
+        'argent',
+        'starkware',
+        'cosmos',
+        'compound',
+        'opyn'
+    },
+    '@multicoincap': {
+        'algorand',
+        'arweave',
+        'dfinity',
+        'dune analytics',
+        'dfinity',
+        'near',
+        'nervos',
+        'skale',
+        'solana',
+        'the graph'
+    },
+    '@dragonfly_cap': {
+        '1inch',
+        'celo',
+        'cosmos',
+        'compound',
+        'dydx',
+        'maker',
+        'opyn',
+        'uma'
+    },
+    '@variantfund': {
+        'uniswap',
+        'reflexer',
+        'cozy finance',
+        'goldfinch'
+    },
+    '@polychain': {
+        '0x',
+        'aleo',
+        'ava labs',
+        'celo',
+        'compound',
+        'dydx',
+        'dfinity',
+        'maker',
+        'cosmos'
+    }
+}
+
 
 def build_api():
     consumer_key = os.getenv('SHILLBOT_CONSUMER_KEY')
@@ -21,6 +72,44 @@ def build_api():
 
     print('built api')
     return api
+
+
+def desc_to_bags(desc):
+    # TODO: are there other examples?
+    tags = [w.lower().rstrip('.,;-!?') for w in desc.split(' ')]
+
+    return dict((tag, FIRM_TO_BAGS[tag]) for tag in tags if tag in FIRM_TO_BAGS)
+
+
+def formatted_bag_tweet(user, bags):
+    # NOTE: basically the output for the tweet itself
+    if len(bags) == 0:
+        return f"couldn't find any bags being shilled by {user.screen_name}. perhaps their firm isn't being tracked by @shillbot yet"
+
+    else:
+        # TODO: format this output nicely
+        ...
+
+
+def get_bags(api, tweet_id):
+    tweet = api.get_status(tweet_id)
+
+    bags = desc_to_bags(tweet.user.description)
+    # TODO: format bag text
+
+    return bags
+
+
+UNSHILL_TEXT = f"{os.getenv('SHILLBOT_HANDLE')} unshill"
+
+
+def get_mentions(api, since_id=1):
+    # NOTE: should be thoughtful about not re-doing replies!
+    mentions = []
+    for tweet in tweepy.Cursor(api.mentions_timeline, since_id=since_id).items():
+        # TODO: if tweet.text == UNSHILL_TEXT: get_shill(api, int(tweet.in_reply_to_status_id_str)), etc.
+        mentions.append(tweet)
+    return mentions
 
 
 if __name__ == '__main__':
